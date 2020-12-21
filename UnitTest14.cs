c5aaf422f9dbf911a5a2a5cff9837a92bfc19b81
Paul Osterhout <posterho@hotmail.com>
2020-12-21 14:24:18 -0500

    Day 14 Part 1

--- UnitTest14.cs
+++ UnitTest14.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +11,34 @@ namespace AdventOfCode2020
         [Fact]
         public void Part1Sample()
         {
-            int answer = 0;
+            InstructionExecuter instructionExecuter = new InstructionExecuter();
+            instructionExecuter.Execute(GetSampleData());
+            long answer = 0;
+            foreach (int key in instructionExecuter.mem.Keys)
+            {
+                answer += instructionExecuter.GetMemValue(key);
+            }
             Assert.Equal(165, answer);
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            InstructionExecuter instructionExecuter = new InstructionExecuter();
+            instructionExecuter.Execute(StringListRetriever.Retreive("INputList14.txt").ToList());
+            long answer = 0;
+            foreach (int key in instructionExecuter.mem.Keys)
+            {
+                answer += instructionExecuter.GetMemValue(key);
+            }
+            Assert.Equal(17481577045893, answer);
         }
 
         private class InstructionExecuter
         {
             internal Dictionary<int, BitArray> mem;
             
-            internal InstructionExecuter(List<string> instructions)
+            internal void Execute(List<string> instructions)
             {
                 mem = new Dictionary<int, BitArray>();
                 string mask = "";
@@ -34,10 +54,15 @@ namespace AdventOfCode2020
                             break;
                         default:
                             int address = int.Parse(assignment.Substring(4, assignment.Length - 5));
-                            BitArray andMask = new BitArray(mask.Replace('X', '1').Select(x => x == 1).ToArray());
-                            BitArray orMask = new BitArray(mask.Replace('X', '0').Select(x => x == 1).ToArray());
-                            BitArray bitValue = new BitArray(value.Select(x => x == '1').ToArray());
-                            BitArray savedValue = bitValue.And(andMask).Or(orMask);
+                            string reverseMask = new string(mask.Reverse().ToArray());
+                            BitArray andMask = new BitArray(reverseMask.Replace('X', '1').Select(x => x == '1').ToArray());
+                            BitArray orMask = new BitArray(reverseMask.Replace('X', '0').Select(x => x == '1').ToArray());
+                            BitArray bitValue = new BitArray(new int[] { int.Parse(value) });
+                            if (bitValue.Length < andMask.Length)
+                            {
+                                bitValue.Length = andMask.Length;
+                            }
+                            BitArray savedValue = bitValue.Or(orMask).And(andMask);
                             if (mem.ContainsKey(address))
                             {
                                 mem[address] = savedValue;
@@ -49,6 +74,18 @@ namespace AdventOfCode2020
                             break;
                     }
                 }
+            }
+
+            internal long GetMemValue(int key)
+            {
+                if (!mem.ContainsKey(key)) return 0;
+                long value = 0;
+                int length = mem[key].Length;
+                for (int index = 0; index < length; index++)
+                {
+                    value += mem[key][index] ? Convert.ToInt64(Math.Pow(2, index)) : 0;
+                }
+                return value;
             }
         }
 
