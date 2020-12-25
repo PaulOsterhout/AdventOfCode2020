4b1d6b6b7e72126dd67c16237b2f9018ca1b7425
Paul Osterhout <posterho@hotmail.com>
2020-12-24 21:51:53 -0500

    Day 18 Part 2

--- UnitTest18.cs
+++ UnitTest18.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -30,52 +31,90 @@ namespace AdventOfCode2020
 		public void Part2Sample()
 		{
 			List<string> formulas = GetPart1SampleData();
-			Assert.Equal(231, FormulaEvaluator.Evaluate(formulas[0]));
-			Assert.Equal(51, FormulaEvaluator.Evaluate(formulas[1]));
-			Assert.Equal(46, FormulaEvaluator.Evaluate(formulas[2]));
-			Assert.Equal(1445, FormulaEvaluator.Evaluate(formulas[3]));
-			Assert.Equal(669060, FormulaEvaluator.Evaluate(formulas[4]));
-			Assert.Equal(23340, FormulaEvaluator.Evaluate(formulas[5]));
+			Assert.Equal(231, FormulaEvaluator.EvaluateAdvanced(formulas[0]));
+			Assert.Equal(51, FormulaEvaluator.EvaluateAdvanced(formulas[1]));
+			Assert.Equal(46, FormulaEvaluator.EvaluateAdvanced(formulas[2]));
+			Assert.Equal(1445, FormulaEvaluator.EvaluateAdvanced(formulas[3]));
+			Assert.Equal(669060, FormulaEvaluator.EvaluateAdvanced(formulas[4]));
+			Assert.Equal(23340, FormulaEvaluator.EvaluateAdvanced(formulas[5]));
+		}
+
+		[Fact]
+		public void Part2()
+		{
+			Assert.Equal(9966990988262, StringListRetriever.Retreive("InputList18.txt").Select(x => FormulaEvaluator.EvaluateAdvanced(x)).Sum());
 		}
 
 		private static class FormulaEvaluator
 		{
 			internal static long Evaluate(string originalFormula)
 			{
+				return PerformEvaluate(EvaluateParenthesis(originalFormula, Evaluate));
+			}
+
+			internal static long EvaluateAdvanced(string originalFormula)
+			{
+				return PerformAdvancedEvaluate(EvaluateParenthesis(originalFormula, EvaluateAdvanced));
+			}
+
+            private static long PerformEvaluate(string originalFormula)
+			{
+				List<string> formulaParts = originalFormula.Split(' ').ToList();
+				while (formulaParts.Count > 1)
+				{
+					string subFormula = $"{formulaParts[0]} {formulaParts[1]} {formulaParts[2]}";
+					formulaParts.RemoveRange(1, 2);
+					formulaParts[0] = $"{EvaluateFormula(subFormula)}";
+				};
+				return long.Parse(formulaParts[0]);
+			}
+
+            private static long PerformAdvancedEvaluate(string originalFormula)
+			{
+				List<string> formulaParts = originalFormula.Split(' ').ToList();
+				while (formulaParts.Count > 1)
+				{
+					int delimiterIndex = formulaParts.IndexOf("+");
+					if (delimiterIndex == -1)
+					{
+						delimiterIndex = 1;
+					}
+					string subFormula = $"{formulaParts[delimiterIndex - 1]} {formulaParts[delimiterIndex]} {formulaParts[delimiterIndex + 1]}";
+					formulaParts.RemoveRange(delimiterIndex, 2);
+					formulaParts[delimiterIndex - 1] = $"{EvaluateFormula(subFormula)}";
+				};
+				return long.Parse(formulaParts[0]);
+			}
+
+			private static long EvaluateFormula(string formula)
+			{
+				string[] formulaParts = formula.Split(' ');
+				long value1 = long.Parse(formulaParts[0]);
+				string operation = formulaParts[1];
+				long value2 = long.Parse(formulaParts[2]);
+				switch (operation)
+				{
+					case "+":
+						return value1 + value2;
+					case "*":
+						return value1 * value2;
+				}
+				throw new Exception($"Bad formula: {formula}");
+			}
+
+			private static string EvaluateParenthesis(string originalFormula, Func<string, long> EvaluteFunction)
+			{
 				string formula = originalFormula;
-				string lastOperation = "+";
-				long result = 0;
 				int startIndex = formula.LastIndexOf('(');
 				while (startIndex > -1)
 				{
 					int length = formula.IndexOf(')', startIndex) - startIndex - 1;
 					string subFormula = formula.Substring(startIndex + 1, length);
-					long subResult = Evaluate(subFormula);
+					long subResult = EvaluteFunction.Invoke(subFormula);
 					formula = $"{formula.Substring(0, startIndex)}{subResult}{formula.Substring(startIndex + length + 2)}";
 					startIndex = formula.LastIndexOf('(');
 				}
-				string[] formulaItems = formula.Split(' ');
-				foreach (string forumlaItem in formulaItems)
-				{
-					long value = 0;
-					if (long.TryParse(forumlaItem, out value))
-					{
-						switch (lastOperation)
-						{
-							case "+":
-								result += value;
-								break;
-							case "*":
-								result *= value;
-								break;
-						}
-					}
-					else
-					{
-						lastOperation = forumlaItem;
-					}
-				}
-				return result;
+				return formula;
 			}
 		}
 
